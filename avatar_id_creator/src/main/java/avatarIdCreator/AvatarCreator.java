e201b16ee3421c9386b9bfea33159d6d5e257edb
Bideep <bbideep@gmail.com>
2018-12-17 17:23:56 -0600

    Update AvatarCreator.java

--- avatar_id_creator/src/main/java/avatarIdCreator/AvatarCreator.java
+++ avatar_id_creator/src/main/java/avatarIdCreator/AvatarCreator.java
@@ -1,13 +1,13 @@
 package avatarIdCreator;
 
-//import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
@@ -16,6 +16,9 @@ import java.util.concurrent.ThreadLocalRandom;
 @Controller
 public class AvatarCreator {
 
+  @Value("${avatarNameUri:ZOMBIE_AVATAR}")
+  private String uri;
+  
   @RequestMapping(value = "/")
   public ModelAndView index() {
     return new ModelAndView("welcome");
@@ -29,10 +32,27 @@ public class AvatarCreator {
     map.put("avatar_id", getAvatarName() + "_" + randomId);
     return new ModelAndView("avatar", map);
   }
-
-  private String getAvatarName() {
+  
+  private String getAvatarName_ONLY_FOR_LOCAL_VALIDATION() {
     String[] names = {"Aang", "Katara", "Sokka", "Zuko", "Iroh", "Appa", "Momo", "Pasang", "Tashi", "Yue" };
     int randomId = ThreadLocalRandom.current().nextInt(0, 10);
     return names[randomId];
   }
+  
+  private String getAvatarName() {
+    String result = null;
+    try {
+      RestTemplate restTemplate = new RestTemplate();
+      result = restTemplate.getForObject(uri, String.class);
+      System.out.println("Result is ::: " + result);
+    } catch (java.lang.Exception e) {
+      //Ignore this bad coding :)
+      if (uri.equals("ZOMBIE_AVATAR")) {
+         result = uri;
+      }
+      System.out.println(e);
+    }
+    return result;
+  }
+
 }
